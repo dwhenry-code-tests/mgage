c992b59ce906ca007503f1ee434fcfa60758c2e2
David Henry <dw_henry@yahoo.com.au>
2014-12-10 23:20:20 +0000

    Add testing around the import script.

---
+++ spec/bin/import_votes_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'import_votes script' do
+  let(:votes_file_path) { File.join(File.dirname(__FILE__), '/votes.txt') }
+
+  context 'when called correctly' do
+    it 'imports and stores data in the database' do
+      `'#{Rails.root}/bin/import_votes' '#{votes_file_path}'`
+
+      expect(Campaign.count).to eq(1)
+      expect(Choice.count).to eq(3)
+    end
+  end
+
+  context 'when called without a filename' do
+    it 'imports and stores data in the database' do
+      output = `'#{Rails.root}/bin/import_votes'`
+
+      expect(output).to eq("USAGE: bin/import_votes <filename>\n")
+    end
+  end
+end
